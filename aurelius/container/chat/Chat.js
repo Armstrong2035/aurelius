0a01b39ed82ffae73077dcd4ba81df113822a16c
Armstrong <armstrongolusoji9@gmail.com>
2024-11-25 17:04:55 +0400

    Loading spinner

--- aurelius/container/chat/Chat.js
+++ aurelius/container/chat/Chat.js
@@ -7,65 +7,73 @@ import {
   Grid2,
   Box,
   Container,
-  AppBar,
-  Toolbar,
   CircularProgress,
+  Backdrop,
 } from "@mui/material";
 import { useChatStore } from "./chatstore";
 
 export default function Chat({}) {
   const chatArray = useChatStore((state) => state.chatArray);
   const isLoading = useChatStore((state) => state.isLoading);
+
   return (
-    <Container
-      sx={{
-        flexGrow: 1, // Take up remaining space
-        display: "flex", // Enable flex layout
-        flexDirection: "column",
-        pb: 10, // Padding bottom to avoid content behind AppBar
-        pt: 2, // Optional top padding
-      }}
-    >
-      <Grid2
-        container
-        direction="column"
-        spacing={1}
+    <>
+      <Backdrop
+        sx={{
+          color: "#fff",
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+        }}
+        open={isLoading}
+      >
+        <CircularProgress color="inherit" size={60} />
+      </Backdrop>
+
+      <Container
         sx={{
           flexGrow: 1,
-          overflowY: "auto",
-          pr: 20,
-          pl: 20,
+          display: "flex",
+          flexDirection: "column",
+          pb: 10,
+          pt: 2,
         }}
       >
-        {chatArray.map((item, index) => (
-          <Grid2 key={index} alignItems="center">
-            {item.type === "system" ? (
-              <SystemChat content={item.content} />
-            ) : (
-              <UserChat content={item.content} />
-            )}
-          </Grid2>
-        ))}
-      </Grid2>
-
-      {isLoading && (
-        <Grid2 alignSelf={"center"} justifySelf={"center"}>
-          <CircularProgress />
+        <Grid2
+          container
+          direction="column"
+          spacing={1}
+          sx={{
+            flexGrow: 1,
+            overflowY: "auto",
+            pr: 20,
+            pl: 20,
+          }}
+        >
+          {chatArray.map((item, index) => (
+            <Grid2 key={index} alignItems="center">
+              {item.type === "system" ? (
+                <SystemChat content={item.content} />
+              ) : (
+                <UserChat content={item.content} />
+              )}
+            </Grid2>
+          ))}
         </Grid2>
-      )}
-      <Box
-        sx={{
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          padding: 2,
-          borderTop: 1,
-          borderColor: "divider",
-        }}
-      >
-        <Prompt />
-      </Box>
-    </Container>
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            padding: 2,
+            bt: 5,
+            borderColor: "divider",
+            bgcolor: "background.paper",
+          }}
+        >
+          <Prompt />
+        </Box>
+      </Container>
+    </>
   );
 }
